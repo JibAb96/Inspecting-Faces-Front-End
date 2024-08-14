0ba116c42d151f76ed21ade3948516f2c188ede3
JibAb96 <j.abshir96@gmail.com>
2024-08-14 13:40:55 +0000

    Add animation effect to background

--- Inspecting-Faces/src/App.js
+++ Inspecting-Faces/src/App.js
@@ -3,10 +3,12 @@ import Navigation from "./components/Navigation/Navigation";
 import tachyons from "tachyons";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkform";
+import Particles from "particles-bg"
 import "./App.css"
 function App() {
   return (
     <div className="App">
+      <Particles  type="cobweb" bg={true} color="#ccffff" num={30}/>
       <Navigation/>
       <Logo/>
       <ImageLinkForm/>
