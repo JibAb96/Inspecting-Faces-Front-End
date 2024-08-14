7e2a8ac442afc6bda55b062159e31c67d4d57bdd
JibAb96 <j.abshir96@gmail.com>
2024-08-14 11:12:41 +0000

    Changed title and description of project

--- Inspecting-Faces/src/App.js
+++ Inspecting-Faces/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navigation from "./components/Navigation/Navigation";
 import tachyons from "tachyons";
 import Logo from "./components/Logo/Logo";
-
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkform";
 function App() {
   return (
     <div>
