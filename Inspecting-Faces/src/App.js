440b0cbf9921b721b0910a3f3b9f732379aa693d
JibAb96 <j.abshir96@gmail.com>
2024-08-15 08:45:08 +0000

    Code Image recognition API and Add Face Recogniton component

--- Inspecting-Faces/src/App.js
+++ Inspecting-Faces/src/App.js
@@ -1,23 +1,99 @@
-import React from "react";
+import {React, Component} from "react";
 import Navigation from "./components/Navigation/Navigation";
 import tachyons from "tachyons";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkform";
 import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/Face Recognition/Face-Recognition";
 import Particles from "particles-bg"
 import "./App.css"
-function App() {
+
+const returnClarifaiRequestOption = (imageURL) => {
+  const PAT = '32b5a7739849494480ba1a6bc8f25fd8';
+  const USER_ID = 'jibab96';
+  const APP_ID = 'Inspecting-Faces';
+  const IMAGE_URL = imageURL;
+
+  const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+
+                  }
+              }
+          }
+      ]
+  });
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+};
+  return requestOptions
+}
+
+class App extends Component{
+  
+  constructor(){
+    super()
+    this.state = {
+      input: "",
+      imageURL: ""
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onSubmit = () => {
+    this.setState({imageURL: this.state.input})
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifaiRequestOption(this.state.input))
+    .then(response => response.json())
+    .then(result => {
+        const regions = result.outputs[0].data.regions;
+
+        regions.forEach(region => {
+            // Accessing and rounding the bounding box values
+            const boundingBox = region.region_info.bounding_box;
+            const topRow = boundingBox.top_row.toFixed(3);
+            const leftCol = boundingBox.left_col.toFixed(3);
+            const bottomRow = boundingBox.bottom_row.toFixed(3);
+            const rightCol = boundingBox.right_col.toFixed(3);
+
+            region.data.concepts.forEach(concept => {
+                // Accessing and rounding the concept value
+                const name = concept.name;
+                const value = concept.value.toFixed(4);
+
+                console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+                
+            });
+        });
+
+    })
+    .catch(error => console.log('error', error));
+  }
+  render() {
   return (
     <div className="App">
       <Particles  type="cobweb" bg={true} color="#ccffff" num={30}/>
       <Navigation/>
       <Logo/>
       <Rank/>
-      <ImageLinkForm/>
-      {/* 
-      <FaceRecognition/>*/}
+      <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+      <FaceRecognition imageURL={this.state.imageURL}/>
     </div>
-  );
+  );}
 }
 
 export default App;
